ba3b8cd090192ee7a2761cf13448a169f6b8017a
MuhammadMazarib <muhammad.mazarib@gmail.com>
2023-01-30 22:48:46 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Tempapp from './Tempapp';
+import Threehour from './Threehour';
+import Sixhour from './Sixhour'
+import Nextday from './Nextday';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ return(<div className='App'>
+
+
+  <div className='Sb'>
+    <Routes>
+      <Route path='/' element={<Tempapp/>}/>
+      <Route path='/threehour' element={<Threehour/>}/>
+      <Route path='/sixhour' element={<Sixhour/>}/>
+      <Route path='/nextday' element={<Nextday/>}/>
+
+    </Routes>
+    
+
+  </div>
+  
+  
+  
+  </div>
   );
-}
+  }
 
 export default App;
---
+++ src/Threehour.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import {useNavigate} from 'react-router-dom'
+import "./css/style.css"
+function Threehour(){
+
+    
+      
+        
+        
+       
+            const [city,setCity]=useState(null);
+            const [iconW,setIconW]=useState(null);
+            const [search,setSearch]=useState("Lahore");
+            const navigate=useNavigate();
+            useEffect(()=>{
+                const fetchApi=async()=>{
+                    const url=`
+                                https://api.openweathermap.org/data/2.5/forecast?q=${search}&units=metric&appid=dc88ae0c527f1b0e7b64ef3130030468
+                    `
+                    const response=await fetch(url);
+                    const resJson=await response.json( );
+                    setCity(resJson.list[1].main);
+                    setIconW(resJson.list[1].weather[0].icon)
+                }
+                fetchApi();
+            },[search])
+            return(
+                
+                <>
+                <div className="box">
+                   <div className="inputData">
+        
+                   <input
+                    type="search"
+                    className="inputfield"
+                    value={search}
+                    onChange={(event)=>{
+                        setSearch(event.target.value)
+        
+                    }}
+                    />
+                   </div>
+                   {
+                    !city?(<p>No data found</p>):(
+        
+        
+                        <div>
+                                    <div className="info">
+                    <h2 className="location">
+                        <i className="fas fa-street-view"></i>{search}
+                    </h2>
+                    
+                    <img src={"http://openweathermap.org/img/wn/"+iconW+"@2x.png"} alt="Error"/>
+                    <h1 className="tmp">{city.temp}</h1>
+        
+                    <h3 className="tmp-min">Min:{city.temp_min} Cel | Max:{city.temp_max} Cel</h3>
+                </div>
+                
+                <button onClick={()=>navigate(-1)}>Back</button>
+                            </div>
+                    )
+        
+                   }
+                    
+        
+                
+        
+                </div>
+                </>
+            )
+        }
+export default Threehour;
